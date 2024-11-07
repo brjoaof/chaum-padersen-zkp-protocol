7975dacbb299c5e916e86600a3783bbf0b7fe634
João Felipe <brjoaof@gmail.com>
2024-11-07 11:34:25 -0300

    feat(lib): add ZKP library functions. test(lib): add unit tests for zkp lib

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,50 @@ pub fn solve(k: &BigUint, c:&BigUint, x:&BigUint, q:&BigUint) -> BigUint {
 // cond1: r1 = alpha^s * y1^c mod p
 // cond2: r2 = beta^s * y2^c mod p
 pub fn verify(r1: &BigUint, r2:&BigUint, alpha: &BigUint, beta:&BigUint, y1:&BigUint, y2:&BigUint, s:&BigUint, c:&BigUint, p:&BigUint) -> bool {
-    let cond1 = *r1 == alpha.modpow(s, p) * y1.modpow(c, p);
-    let cond2 = *r2 == beta.modpow(s, p) * y2.modpow(c, p);
+    let cond1 = *r1 == (alpha.modpow(s, p) * y1.modpow(c, p)).modpow(&BigUint::from(1u32), p);
+    let cond2 = *r2 == (beta.modpow(s, p) * y2.modpow(c, p)).modpow(&BigUint::from(1u32), p);
 
     cond1 && cond2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        let alpha = BigUint::from(4u32);
+        let beta = BigUint::from(9u32);
+        let p = BigUint::from(23u32);
+        let q = BigUint::from(11u32);
+
+        let x = BigUint::from(6u32);
+        let k = BigUint::from(7u32);
+
+        let c: BigUint = BigUint::from(4u32);
+
+        let y1 = exponetiate(&alpha, &x, &p);
+        let y2 = exponetiate(&beta, &x, &p);
+        assert_eq!(y1, BigUint::from(2u32));
+        assert_eq!(y2, BigUint::from(3u32));
+
+        let r1 = exponetiate(&alpha, &k, &p);
+        let r2 = exponetiate(&beta, &k, &p);
+        assert_eq!(r1, BigUint::from(8u32));
+        assert_eq!(r2, BigUint::from(4u32));
+
+        let s = solve(&k, &c, &x, &q);
+        assert_eq!(s, BigUint::from(5u32));
+
+        let result = verify(&r1, &r2, &alpha, &beta, &y1, &y2, &s, &c, &p);
+        assert!(result);
+
+        // Fake Secret
+        let x_fake = BigUint::from(7u32);
+        let s_fake = solve(&k, &c, &x_fake, &q);
+
+        let result = verify(&r1, &r2, &alpha, &beta, &y1, &y2, &s_fake, &c, &p);
+        assert!(!result);
+
+    }
 }
